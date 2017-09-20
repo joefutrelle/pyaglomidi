4e7a02fc730a9aea70cd51601e3bd7d7dab6334b
Joe Futrelle <joefutrelle@gmail.com>
2017-09-20 08:26:10 -0400

    1) parameters, 2) one-line min

--- rt.py
+++ rt.py
@@ -13,22 +13,14 @@ class RtQueue(object):
     def __init__(self):
         self.id = 0
         self.q = {}
-    def schedule(self, callback, t):
-        self.q[self.id] = (callback, t)
+    def schedule(self, callback, t, params=None):
+        self.q[self.id] = (callback, t, params)
         self.id += 1
-    def schedule_in(self, callback, ins):
+    def schedule_in(self, callback, ins, params=None):
         self.schedule(callback, in_s(ins))
     def call_next(self):
-        min_t = now() + now() # a long time
-        min_k = None
-        for k,v in self.q.items():
-            _, t = v
-            if t < min_t:
-                min_t = t
-                min_k = k
-        if min_k is None:
-            return
-        callback, t = self.q[min_k]
+        min_k, _ = min(self.q.items(), key=lambda i: i[1][1])
+        callback, t, params = self.q[min_k]
         del self.q[min_k]
         wait_t = t - now()
         wait(wait_t)
